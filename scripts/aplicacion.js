98194c1b5a07c521f70687dcb8137243120546b2
rgmagadan <rgmagadan@yahoo.es>
2022-12-08 23:56:55 +0100

    Elimina marcador.

--- scripts/aplicacion.js
+++ scripts/aplicacion.js
@@ -2,8 +2,10 @@ let partida;
 
 function empezarPartida() {
   const tablero = document.querySelector("#tablero");
+  const marcador = document.querySelector("#marcador");
   if (tablero) {
     document.querySelector('#container').removeChild(tablero);
+    document.querySelector('#container').removeChild(marcador);
   }
   empezar.disabled = true;
   let jugadorX = new Jugador(document.getElementById("X"));
