af0d0e55c41ae0391ef2b24f212b8d8c6dd618c5
rgmagadan <rgmagadan@yahoo.es>
2022-12-08 01:15:40 +0100

    Cambia info partida

--- scripts/partida.js
+++ scripts/partida.js
@@ -48,7 +48,7 @@ class Partida {
       document.querySelector("#victoria").play();
       setTimeout(
         () =>
-          (marcador.innerHTML = `<span id="jugada">${idCasilla}</span><span id="infoPartida">${this._jugadorQueTieneElTurno.ficha} ha ganado la partida.</span>`),
+          (marcador.innerHTML = `<span id="jugada">${idCasilla}</span><span id="infoPartida">${this._jugadorQueTieneElTurno.ficha} ha ganado.</span>`),
         500
       );
       empezar.disabled = false;
@@ -56,7 +56,7 @@ class Partida {
       document.querySelector("#empate").play();
       setTimeout(
         () =>
-          (marcador.innerHTML = `<span id="jugada">${idCasilla}</span><span id="infoPartida">La partida ha terminado en empate.</span>`),
+          (marcador.innerHTML = `<span id="jugada">${idCasilla}</span><span id="infoPartida">Partida terminada en empate.</span>`),
         100
       );
       empezar.disabled = false;
